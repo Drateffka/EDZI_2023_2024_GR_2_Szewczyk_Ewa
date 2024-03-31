209853c6416244c6b8155fb0c63e1b02448705d8
Ewa Szewczyk <ewa.z.szewczyk@gmail.com>
2024-03-31 15:00:49 +0200

    finished?

--- Projekt_1/run.py
+++ Projekt_1/run.py
@@ -1,9 +1,15 @@
 from src.crawlers import PracujCrawler, JustJoinCrawler
 from src.offers import Offer
+from src.analisis import (
+    skills_counter,
+    positions_counter,
+    save_results_one_json,
+    skills_plotter,
+    positions_plotter,
+)
 import os
 
 
-# 5h
 def main():
     # If there is a need to rerun the scraping - delete file
     if not os.path.exists("results/offers.json"):
@@ -23,6 +29,15 @@ def main():
         pc_offers.save_json("results/offers.json")
     else:
         df = Offer().read_json("results/offers.json")
+
+    skills_count = skills_counter(df["skills/technologies"])
+    positions_count = positions_counter(df)
+
+    save_results_one_json(skills_count, positions_count)
+
+    skills_plotter(skills_count)
+
+    positions_plotter(positions_count)
 
 
 if __name__ == "__main__":
